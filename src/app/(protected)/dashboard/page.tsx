e3bc73b85f620ba60fbb1384552598404b21e0dc
Tristan Gerber <tristan.gerber@etu.unistra.fr>
2024-12-06 23:38:22 +0100

    starting dashboard page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,13 +1,28 @@
 "use client";
 import React from 'react';
 import { useUser } from '@clerk/nextjs';
+import useProjects from '@/hooks/use-projects';
+import { ExternalLink, Github } from 'lucide-react';
+import Link from 'next/link';
 
 const Dashboard = () => {
-    const { user } = useUser();
+    const {project} = useProjects()
     return (
         <div>
-            {/* {user?.imageUrl && <img src={user.imageUrl} alt="User Image" />} */}
-            <div>{user?.firstName}</div>
+            <div className='flex items-center justify-between flex-wrap gap-y-4'>
+                <div className='w-fit rounded-md bg-primary px-4 py-3 flex items-center'>
+                    <Github className='size-5 text-white'   />
+                    <div className='ml-2'>
+                        <p className='text-sm font-medium text-white'>
+                            This project is linked to {' '}
+                            <Link href={project?.githubUrl ?? ""} className='inline-flex items-center text-white/80 hover:underline'>
+                               {project?.githubUrl}
+                               <ExternalLink className='size-4 ml-1' />
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
